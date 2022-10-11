dd29819af603d96b41fbd8261eb6c9f040214764
Laslau-Doru-Mihai <alulas5849@ieselcaminas.orh>
2022-10-11 20:24:27 +0200

    Initial commit

---
+++ src/Example18.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Example18 {
+    public static void main(String[] argv) {
+        double pricePerHour, numberOfHours, grossPay, taxes, netPay;
+
+        Scanner inputValue = new Scanner(System.in);
+
+        System.out.println("Enter number of hours:");
+        numberOfHours = inputValue.nextFloat();
+
+        System.out.println("Enter price per hour:");
+        pricePerHour = inputValue.nextFloat();
+
+        if (numberOfHours > 35) {
+            grossPay = 35 * pricePerHour + (numberOfHours - 35) * 1.5;
+        } else {
+            grossPay = numberOfHours * pricePerHour;
+        }
+        System.out.println("Gross salary:"+grossPay);
+
+        if (grossPay>500) {
+            if (grossPay>900) {
+                taxes = 400*0.25 + (grossPay-900)*0.45;
+            } else {
+                taxes=(grossPay-500)*0.25;
+            }
+        } else {
+            taxes=0;
+        }
+
+        netPay=grossPay-taxes;
+        System.out.println("Taxes are:"+taxes);
+        System.out.println("Net pay is:"+netPay);
+    }
+}
